a7cef3fc22d8fef927933a07c9bc910fed6f7de1
Victoria Amirov <vika.amir20@gmail.com>
2023-08-05 21:27:36 -0700

    change file

--- 4_queries/Average_Cohort_Assistance_Time.sql
+++ 4_queries/Average_Cohort_Assistance_Time.sql
@@ -1,2 +1,5 @@
-SELECT avg(started_at-created_at) as average_wait_time
-FROM assistance_requests;
+SELECT cohorts.name as name, AVG(completed_at - started_at) as average_assistance_time
+FROM cohorts
+JOIN assistance_requests on assistance_requests.id = cohorts.id
+GROUP BY name
+ORDER BY average_assistance_time;
